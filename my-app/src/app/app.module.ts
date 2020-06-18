c590678ff23ca6a7ef4f735b45d08432582fc03e
Reza Fitriaman <rezafitriaman@gmail.com>
2020-06-18 21:07:56 +0200

    leason 34

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from "./server/server.component";
@@ -17,7 +18,8 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
     WarningAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/servers/servers.component.ts
+++ my-app/src/app/servers/servers.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!'
+  serverName = 'TestServer';
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
@@ -16,4 +18,12 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    console.log(event)
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
