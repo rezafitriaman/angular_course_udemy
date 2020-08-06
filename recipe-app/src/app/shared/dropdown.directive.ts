efd23a44499f24cd92040d362a59cbad805cee39
Reza Fitriaman <rezafitriaman@gmail.com>
2020-08-06 22:35:15 +0200

    finsihed course 8

---
+++ recipe-app/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, Renderer2, HostListener, Input, HostBinding, OnInit } from '@angular/core';
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective implements OnInit{
+    @Input() addShowClass: boolean = false;
+
+    @HostBinding('class.show') isShown: boolean = this.addShowClass;
+    
+    constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+    
+    ngOnInit() {
+        this.isShown = this.addShowClass;
+    }
+
+    @HostListener('document:click', ['$event']) toggleShow(eventData: Event) {
+        console.log(this.elRef.nativeElement)
+        this.isShown = this.elRef.nativeElement.contains(eventData.target) ? !this.isShown : false;
+        this.addClassToChild();
+    }
+
+    addClassToChild() {
+        const action = this.isShown ? 'addClass' : 'removeClass';
+        //todo look up queryselector for angular
+        this.renderer[action](this.elRef.nativeElement.querySelector('.dropdown-menu'), 'show');
+    }
+}
