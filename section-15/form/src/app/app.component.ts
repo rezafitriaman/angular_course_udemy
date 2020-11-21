6aa7cdea1f548c04b415ba5f947e0001f1fd0093
rezafitriaman <rezafitriaman@gmail.com>
2020-11-21 21:27:48 +0100

    add form

---
+++ section-15/form/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm | undefined
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    /* this.signupForm?.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    }) */
+    this.signupForm?.form.patchValue({
+      userData: {
+        username: suggestedName
+      },
+    })
+  }
+  /* onSubmit(form: NgForm) {
+    console.log('submitted')
+    console.log(form)
+  } */
+  onSubmit() {
+    console.log(this.signupForm)
+  }
+}
+
+// TODO 198 using form data
