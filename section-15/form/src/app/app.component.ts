9db835121bce02b50a2c1fb3363cf37e6fb9e383
rezafitriaman <rezafitriaman@gmail.com>
2020-11-22 14:23:59 +0100

    finished template driven form

--- section-15/form/src/app/app.component.ts
+++ section-15/form/src/app/app.component.ts
@@ -12,6 +12,15 @@ export class AppComponent {
   answer = '';
   genders = ['male', 'female'];
 
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+
+  submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
     /* this.signupForm?.setValue({
@@ -34,7 +43,15 @@ export class AppComponent {
     console.log(form)
   } */
   onSubmit() {
+    this.submitted = true
     console.log(this.signupForm)
+    this.user.username = this.signupForm?.value.userData.username;
+    this.user.email = this.signupForm?.value.userData.email;
+    this.user.secretQuestion = this.signupForm?.value.secret;
+    this.user.answer = this.signupForm?.value.questionAnswer;
+    this.user.gender = this.signupForm?.value.gender;
+
+    this.signupForm?.reset();
   }
 }
 
